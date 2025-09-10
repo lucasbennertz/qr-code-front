eab64e5a2ec68a9dab3baab610cb92e6ddaf9dc5
lucasbennertz <lucasgcbennertz@gmail.com>
2025-09-10 20:08:29 -0300

    criação do campo hero

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { MainScreenComponent } from './screens/main-screen/main-screen.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "", component: MainScreenComponent, pathMatch: "full"}
+];
--- src/app/screens/main-screen/main-screen.component.ts
+++ src/app/screens/main-screen/main-screen.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { HeroComponent } from '../../components/hero/hero.component';
 
 @Component({
   selector: 'app-main-screen',
   standalone: true,
-  imports: [],
+  imports: [
+    HeroComponent,
+  ],
   templateUrl: './main-screen.component.html',
   styleUrl: './main-screen.component.scss'
 })

7eef4c6d9eadcbba4285fd84831a1855145284e9
lucasbennertz <lucasgcbennertz@gmail.com>
2025-09-14 15:07:08 -0300

    criação de cards

--- src/app/screens/main-screen/main-screen.component.ts
+++ src/app/screens/main-screen/main-screen.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from '../../components/hero/hero.component';
+import { CardsComponent } from '../../components/cards/cards.component';
 
 @Component({
   selector: 'app-main-screen',
   standalone: true,
   imports: [
     HeroComponent,
+    CardsComponent
   ],
   templateUrl: './main-screen.component.html',
   styleUrl: './main-screen.component.scss'
